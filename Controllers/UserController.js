9b6505ee6f3a7130994ce99ec9aacefcfdc337ce
Rishiraj Kundu <54744734+Rishiraj-Kundu@users.noreply.github.com>
2023-11-19 22:16:48 +0530

    Update UserController.js

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -55,11 +55,7 @@ module.exports.removeFromLikedMoives = async (req, res) => {
       );
       return res.json({ msg: "Movie deleted", movies: likedMovies });
     }
-  } catch (err) {
-<<<<<<< HEAD
-    return; 
-=======
+  } catch (err) { 
     return;// res.json({ msg: "Error deleting movie" });
->>>>>>> 979cb908158ea92ce62b4053406f57fa796d4ae2
   }
 };
